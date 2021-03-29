aacfe38d3e70e5c274099296d226272e80ee0d1e
root <root@hugboy.127.0.0.1>
2021-03-29 13:30:40 +0800

    delete useless DiscuzX

--- html/discuzx/data/log/202102_sendmail.php
+++
@@ -1,2 +0,0 @@
-<?PHP exit;?>	2021-02-25 15:09:54	39.149.93.149	0	/discuzx/member.php?mod=lostpasswd&lostpwsubmit=yes&infloat=yes&inajax=1	2263202422@qq.com sendmail failed.
-
--- html/discuzx/uc_server/data/logs/202102.php
+++
@@ -1,2 +0,0 @@
-<?PHP exit;?>	UCenterAdministrator	39.149.82.32	1614177492	login	succeed
-<?PHP exit;?>	UCenterAdministrator	39.149.93.149	1614179447	login	succeed
